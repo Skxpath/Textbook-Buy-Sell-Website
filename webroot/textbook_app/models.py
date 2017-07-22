50b84f6f880b0827383c0383cb847fe8f156ed8b
Graeme <gds3@sfu.ca>
2017-07-21 18:38:37 -0700

    Use django ModelForms

--- webroot/textbook_app/models.py
+++ webroot/textbook_app/models.py
@@ -1,15 +1,24 @@
 from django.db import models
 from django.conf import settings
+from django.forms import ModelForm
+from django import forms
 
 MAX_ISBN_DIGITS = 13
 MAX_PRICE_DOLLAR_DIGITS = 4
 PRICE_CENTS_DIGITS = 2
 
 class Textbook(models.Model):
-    title = models.CharField(max_length=200)
-    author = models.CharField(max_length=200)
-    description = models.TextField()
-    isbn = models.DecimalField(max_digits=MAX_ISBN_DIGITS, decimal_places=0, primary_key=True)
+    isbn = models.DecimalField('ISBN', max_digits=MAX_ISBN_DIGITS, decimal_places=0, primary_key=True)
+    title = models.CharField('Book Title', max_length=200)
+    author = models.CharField('Author', max_length=200)
+    description = models.TextField('Description')
+
+class TextbookForm(ModelForm):
+    description = forms.CharField(required=False, widget=forms.Textarea)
+
+    class Meta:
+        model = Textbook
+        fields = '__all__'
 
 class Ad(models.Model):
     price = models.DecimalField(max_digits=MAX_PRICE_DOLLAR_DIGITS + PRICE_CENTS_DIGITS, decimal_places=PRICE_CENTS_DIGITS)
@@ -29,7 +38,8 @@ class Ad(models.Model):
     condition = models.CharField(
         max_length=MAX_BOOK_CONDITION_CODE_LENGTH,
         choices=BOOK_CONDITIONS,
-        default=GOOD
+        default=GOOD,
+        blank=False
     )
     # Forms a many-to-one relationship between users and ads
     # i.e. an ad belongs to only one user, but a user can have many ads
@@ -37,3 +47,12 @@ class Ad(models.Model):
     poster = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     # We want to ensure that every ad is associated with a Textbook, so all ads for a textbook can be found together
     book = models.ForeignKey(Textbook, on_delete=models.CASCADE)
+
+class AdForm(ModelForm):
+    class Meta:
+        model = Ad
+        exclude = ['poster', 'book']
+
+
+
+
--- webroot/textbook_app/views.py
+++ webroot/textbook_app/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.decorators import login_required
-from .models import Ad, Textbook
+from .models import Ad, Textbook, AdForm, TextbookForm
 from django.urls import reverse
 from django.http import HttpResponseRedirect
 
@@ -32,27 +32,24 @@ def profile(request):
 # Once implemented properly, the user should be able to search through existing textbooks
 # and link that textbook to their ad if a textbook already exists. Otherwise, they can add a new one
 @login_required
-def ads_new_page(request):
-    return render(request, 'textbook_app/ads_new.html')
-
-@login_required
-def ads_new_request(request):
-    textbook = Textbook(
-        title = request.POST['title'],
-        author = request.POST['author'],
-        description = request.POST['description'],
-        isbn = request.POST['isbn'],
-    )
-    textbook.save()
-    ad = Ad(
-        # TODO: don't hardcode values here!
-        price = 3.50,
-        condition = 'GOOD',
-        book = textbook,
-        poster = request.user,
-    )
-    ad.save()
-    return HttpResponseRedirect(reverse('textbook_app:ads'))
+def ads_new(request):
+    if request.method == 'POST':
+        textbookForm = TextbookForm(request.POST)
+        adForm = AdForm(request.POST)
+        textbook_valid = textbookForm.is_valid()
+        ad_valid = adForm.is_valid()
+        # Do this because 'and' short circuits and we want to check both for validity
+        if textbook_valid and ad_valid:
+            textbook = textbookForm.save()
+            ad = adForm.save(commit=False)
+            ad.book = textbook
+            ad.poster = request.user
+            ad.save()
+            return HttpResponseRedirect(reverse('textbook_app:ads'))
+    else:
+        textbookForm = TextbookForm()
+        adForm = AdForm()
+    return render(request, 'textbook_app/ads_new.html', {'textbookForm': textbookForm, 'adForm': adForm})
 
 def textbook_edit(request, textbook_isbn):
     if request.method == "POST":
