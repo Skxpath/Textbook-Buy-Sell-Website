52ce2141c14671a83781cd7b66cd6a867f9efb3f
Trevor Clelland <trevy17@gmail.com>
2017-07-24 20:34:14 -0700

    Allow Users to Either Select Existing Textbooks For Ads, Or Create A New Textbook
    - Add ability to toggle visibility of new textbook form
    - Modify ad_new view to display server side input validation errors to the
    user (as opposed to just not returning a response)
    - Make the ad_new view support creation of an ad with an existing
    textbook

--- webroot/textbook_app/static/ad_new.js
+++ webroot/textbook_app/static/ad_new.js
@@ -1,19 +1,3 @@
-// Call this function when the page loads
-$(function() {
-  $('#search').keyup(function() {
-    $.ajax({
-      type: 'POST',
-      url: '/textbook/textbook_search',
-      data: {
-        'search_text': $('#search').val(),
-        'csrfmiddlewaretoken': $('input[name=csrfmiddlewaretoken]').val(),
-      },
-      success: searchSuccess,
-      dataType: 'html',
-    });
-  });
-});
-
 function searchSuccess(data, textStatus, jqXHR) {
   $('#search_results').html(data);
 }
@@ -21,7 +5,25 @@ function searchSuccess(data, textStatus, jqXHR) {
 var vm = new Vue({
   el: '#app',
   data: {
-    message: 'Hello Vue!'
+    message: 'Hello Vue!',
+    isTextbookFormShown: false,
+  },
+  methods: {
+    toggleTextbookForm: function() {
+      this.isTextbookFormShown = !this.isTextbookFormShown;
+    },
+    searchTextbooks: function() {
+      $.ajax({
+        type: 'POST',
+        url: '/textbook/textbook_search',
+        data: {
+          'search_text': $('#search').val(),
+          'csrfmiddlewaretoken': $('input[name=csrfmiddlewaretoken]').val(),
+        },
+        success: searchSuccess,
+        dataType: 'html',
+      });
+    },
   },
   // Lets us use vue templates with different brackets. Ex: [data] instead of the default {{data}}
   // This is because the default brackets conflicts with django's templating system
--- webroot/textbook_app/urls.py
+++ webroot/textbook_app/urls.py
@@ -9,8 +9,7 @@ urlpatterns = [
     url(r'^ads/(?P<ad_id>[0-9]+)/edit', views.ad_edit, name='ad_edit'),
     url(r'^ads/(?P<ad_id>[0-9]+)', views.ad_detail, name='ad_detail'),
     url(r'^profile/$', views.profile, name='profile'),
-    url(r'^ads_new$', views.ads_new, name='ads_new'),
+    url(r'^ad_new$', views.ad_new, name='ad_new'),
     url(r'^(?P<textbook_isbn>[0-9]+)/edit', views.textbook_edit, name='textbook_edit'),
-    url(r'^ads_new$', views.ads_new, name='ads_new'),
     url(r'^textbook_search$', views.textbook_search, name='textbook_search')
 ]
