3624e9d4e3d98ed5d194797930cd6a93e1a027ec
Graeme <gds3@sfu.ca>
2017-07-30 16:33:28 -0700

    Replace sqlite with postgresql and add some initial data

--- webroot/term_project/settings.py
+++ webroot/term_project/settings.py
@@ -77,8 +77,9 @@ WSGI_APPLICATION = 'term_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'mydb',
+        'USER': 'ubuntu',
     }
 }
 
