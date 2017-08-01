0db2e1406fdc410eb3330aafb6ee68fc0c5849bd
Trevor Clelland <trevy17@gmail.com>
2017-08-01 00:35:13 -0700

    Implement Basic Chat Functionality
    - Implement Chat functionality that users can access by viewing the Ad
    Details Page
    - Add message model and chat model
    - messages have a many to one relationship with chats
    - Chats have a many to many relationship with users. This way, a chat
    can be associated to multiple users (two people involved in the same
    chat), and users can have multiple chats associated with their account
    (i.e. multiple chats with different users)
    - Right now, the system is designed so there should only be one chat
    between two users (unless a race condition bug is exploited, in which
    case multiple may form)
    - Styling of the message page could be improved... Maybe next commit

--- webroot/textbook_app/urls.py
+++ webroot/textbook_app/urls.py
@@ -12,4 +12,6 @@ urlpatterns = [
     url(r'^ad_new$', views.ad_new, name='ad_new'),
     url(r'^(?P<textbook_isbn>[0-9]+)/edit', views.textbook_edit, name='textbook_edit'),
     url(r'^textbook_search$', views.textbook_search, name='textbook_search'),
+    url(r'^chat/(?P<receiver_user_id>[\w-]{,50})/$', views.chat, name='chat'),
+    url(r'chat/send$', views.chat_send_message, name='chat_send_message')
 ]
