d8e4e5444385b70c9733820fb01c25908c6110f6
Trevor Clelland <trevy17@gmail.com>
2017-08-02 12:56:33 -0700

    Remove Email Verification From Signup Process
    - Remove views from signup app associated with email verification
    - Remove urls for those views
    - Modify signup view to redirect user to their new profile page after
    successful signup

--- webroot/accounts/urls.py
+++ webroot/accounts/urls.py
@@ -10,9 +10,6 @@ urlpatterns = [
     url(r'^login/$', auth_views.login, kwargs={'template_name': 'accounts/login.html'}, name='login'),
     # When the user logs out, go back to the login page
     url(r'^logout/$', auth_views.logout,  {'next_page': 'accounts:login'}, name='logout'),
-    url(r'^account_activation_sent/$', account_views.account_activation_sent, name='account_activation_sent'),
-    url(r'^activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
-    account_views.activate, name='activate'),
     url(r'^password_reset/$', auth_views.password_reset, {'post_reset_redirect' : 'accounts:password_reset_done',}, name='password_reset'),
     url(r'^password_reset/done/$', auth_views.password_reset_done, name='password_reset_done'),
     url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
